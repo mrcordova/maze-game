cfaf76bdb88c70583cd0c49af88f3513554492ca
mrcordova <46950394+mrcordova@users.noreply.github.com>
2025-02-01 16:52:12 -0800

    add tests

--- maze.py
+++ maze.py
@@ -1,7 +1,7 @@
 import time
 from cell import Cell
 class Maze:
-    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win):
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win = None):
         self._cells = []
         self._x1 = x1
         self._y1 = y1
